cd7e45bd08731bfe2c618a19b7f6b4efb78b3ab6
Benjamin Barault <benjaminbarault@gmail.com>
2017-01-29 23:48:38 -0500

    Trying to setup the frontend

--- frontend/app.routes.js
+++ frontend/app.routes.js
@@ -2,7 +2,7 @@
 	angular.module('main').config(['$stateProvider', function($stateProvider) {
 		$stateProvider.state('test', {
 			url: '/test',
-			templateUrl: 'templates/test.html'
+			templateUrl: '/templates/test.html'
 		});
 	}]);
 })(angular);
