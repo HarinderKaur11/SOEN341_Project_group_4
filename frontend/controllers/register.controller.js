a062ce19849e29a5f0336e1a6e0e962cbab9a435
shelbaz <elbaz.shawn@gmail.com>
2017-01-31 20:10:25 -0500

    Basic register controller

---
+++ frontend/controllers/register.controller.js
@@ -0,0 +1,32 @@
+(function () {
+
+  angular
+    .module('main')
+    .controller('registerCtrl', registerCtrl);
+
+  registerCtrl.$inject = ['$location', 'authentication'];
+  function registerCtrl($location, authentication) {
+    var vm = this;
+
+    vm.credentials = {
+      id : "",
+      username : "",
+      password : "",
+      type: ""
+    };
+
+    vm.onSubmit = function () {
+      console.log('Submitting registration');
+      authentication
+        .register(vm.credentials)
+        .error(function(err){
+          alert(err);
+        })
+        .then(function(){
+          $location.path('profile');
+        });
+    };
+
+  }
+
+})();
