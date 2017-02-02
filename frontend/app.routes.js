eac44cf9606b89fcb0fb4b1b5330df79e2d75d94
Benjamin <benjaminbarault@gmail.com>
2017-02-01 21:40:39 -0500

    Moved login template and linked it with home page

--- frontend/app.routes.js
+++ frontend/app.routes.js
@@ -2,7 +2,12 @@
 	angular.module('main').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
         $stateProvider.state('home', {
             url: '/home',
-            templateUrl: '/templates/homepage.html'
+            templateUrl: '/templates/homepage.template.html'
+        });
+
+        $stateProvider.state('login', {
+            url: '/login',
+            templateUrl: '/templates/login.template.html'
         });
 
 		$urlRouterProvider.otherwise('/home');
