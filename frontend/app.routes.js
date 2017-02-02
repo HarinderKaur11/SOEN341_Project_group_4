3c1fc2abf2ff047b786fbe967dce94ea57392c0b
Benjamin <benjaminbarault@gmail.com>
2017-02-01 19:32:42 -0500

    Removed test.html template

--- frontend/app.routes.js
+++ frontend/app.routes.js
@@ -1,10 +1,5 @@
 (function(angular) {
 	angular.module('main').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-		$stateProvider.state('test', {
-			url: '/test',
-			templateUrl: '/templates/test.html'
-		});
-
         $stateProvider.state('home', {
             url: '/home',
             templateUrl: '/templates/homepage.html'
