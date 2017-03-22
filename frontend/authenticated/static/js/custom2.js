111d0df69a0adc35ebe7acc8e64c946cb0236e54
Radu <elbaz.shawn@gmail.com>
2017-03-21 23:31:12 -0400

    Attempt to display chatbox

--- frontend/authenticated/static/js/custom2.js
+++ frontend/authenticated/static/js/custom2.js
@@ -1,11 +1,11 @@
-window.onload= function() {
+function showCloudilly() {
     window.cloudilly= new Cloudilly();
     cloudilly.initialize("SOEN341", "73ef8dcf-26c6-4b98-b130-3b5976b3128e", function() {
         cloudilly.connect();	
     });
 
     cloudilly.socketConnected(function(err, res) {
-        if(err) { console.log("ERROR: Oops. Something wrong"); return; }
+        
         console.log("@@@@@@ CONNECTED");
         console.log(res); 
     });
