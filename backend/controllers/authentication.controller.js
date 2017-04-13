8baeffd7f4f9c688f146a1dd163d26846e03d45b
Klaipar <miguelangel_mb@hotmail.com>
2017-04-13 14:21:18 -0400

    Update authentication.controller.js

--- backend/controllers/authentication.controller.js
+++ backend/controllers/authentication.controller.js
@@ -20,33 +20,33 @@ exports.register = function (req, res) {
 
     if (req.body.name && req.body.username && req.body.password) {
         global.models.userModel.findOne({ username: req.body.username }, function (err, user) {
-            if (err) {
-                response.error = "Invalid request.";
-                res.status(400).json(response);
+            if (err) { 
+                response.error = "Invalid request."; 
+                res.status(400).json(response); 
             } else {
-                if (user) {
-                    response.error = "Username already taken.";
-                    res.status(400).json(response);
-                } else {
-                    var user = new global.models.userModel({
-                        name: req.body.name,
-                        username: req.body.username,
-                        password: bcrypt.hashSync(req.body.password, saltR),
-                        type: 'student',
+                if (user) { 
+                    response.error = "Username already taken."; 
+                    res.status(400).json(response); 
+                } else { 
+                    var user = new global.models.userModel({ 
+                        name: req.body.name, 
+                        username: req.body.username, 
+                        password: bcrypt.hashSync(req.body.password, saltR), 
+                        type: 'student',  
                         courses: []
                     });
-
+ 
                     user.save(function (err, user) {
-                        if (!err) {
-                            req.login(user, function (err) {
+                        if (!err) { 
+                            req.login(user, function (err) {  
                                 if (err) {
-                                    response.error = "Could not login after creating user.";
+                                    response.error = "Could not login after creating user.";    
                                     res.status(400).json(response);
-                                }
+                                }    
                                 response.success = "Successfully created the user.";
                                 res.json(response);
                             });
-                        } else {
+                        } else { 
                             response.error = "Could not save user, try again.";
                             res.status(400).json(response);
                         }
@@ -54,20 +54,20 @@ exports.register = function (req, res) {
                 }
             }
         });
-    } else {
+    } else { 
         response.error = "Invalid request parameters.";
         res.status(400).json(response);
-    }
+    } 
 };
-
+ 
 exports.getAccountType = function (req, res) {
-    var response = {};
-
-    if (req.user) {
-        response.type = req.user.type;
-        res.json(response);
+    var response = {}; 
+ 
+    if (req.user) { 
+        response.type = req.user.type; 
+        res.json(response); 
     } else {
         response.error = "You are not signed in.";
         res.status(400).json(response);
     }
-};
+};        
